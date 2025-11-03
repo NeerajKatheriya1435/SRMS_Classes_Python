46de48d67c62f08db97d0a01e3411fc08184ba0a
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-03 13:13:32 +0530

    Day 1 code

---
+++ day1.py
@@ -0,0 +1,31 @@
+# this ios for printing hello world
+# print("Hello world")
+
+#gjhugjujuhj
+
+# """this is my multi line 
+# comments
+# in python"""
+# def greet():
+#     print("ehlklo")
+
+# greet()
+
+print("hello' \tmohan \nthis  is my next\"s  line")
+
+a=7
+MohanKumar=89
+var1=89
+# 3var=67
+# print(3var)
+Var1=8
+print(var1)
+print(Var1)
+print(a)
+f1=8.98
+print(f1)
+b1=True
+print(b1)
+
+str1="hello"
+print(str1)
