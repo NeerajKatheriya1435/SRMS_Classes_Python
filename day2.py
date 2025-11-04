c576cfe8578ac9527e392370045311726c6c98ac
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-04 15:46:33 +0530

    Updated code for day 2

---
+++ day2.py
@@ -0,0 +1,142 @@
+# str1="aman"
+# str2="kumar"
+# print(str1,str2,sep=" and ",end="  ")
+# print("Shivam")
+# print(str2)
+
+# name=input("Enter Your name: ")
+# age=input("Enter Your age: ")
+# course=input("Enter Your Course: ")
+
+# print("My name is: ",name)
+# print("My age is: ",age)
+# print("My Course is: ",course)
+
+# multiline="""my name is shann" 
+# my age is 56
+# course is B.tech
+# """
+# print(multiline)
+
+# str1="Aman"
+
+# l1=[8,9,5]
+# l1[1]=99
+# for item in l1:
+#     print(item)
+
+# print(l1)
+
+# str1="    Aman Kumar Yadav    "
+# str1[1]="h"
+# for charater in str1:
+#     print(charater)
+
+# print(str1.upper())
+# print(str1.lower())
+# print(str1)
+# print(str1.strip())
+
+
+# age=int(input("Enter your age"))
+
+# if(age>18):
+#     print("You can drive the car")
+
+# else:
+#     print("You can not drive the car")
+
+# inp1=int(input("Enter the number: "))
+
+# if(inp1>0):
+#     print("Number is Positive")
+
+# elif (inp1==0):
+#     print("Numver is Zero")
+# else:
+#     print("Number is Negative")
+
+# print(7>8 or 5>8)
+
+# value=78
+# match value:
+#     case 1:
+#         print("Sunday")
+#     case 2:
+#         print("Monday")
+#     case 3:
+#         print("Tuesday")
+#     case _:
+#         print("Deafualt")
+
+# num1=int(input("Enter the num1: "))
+# num2=int(input("Enter the num2: "))
+# op1=input("Enter the operator (*,+,-,/): ")
+
+# match op1:
+#     case "+":
+#         print("The sum is: ",(num1+num2))
+#     case 
+
+# for i in range(5,11,2):
+#     print(i,end=" ")
+
+# inp1=int(input("Enter the number you want series"))
+
+# for i in range(inp1):
+#     print(i,end=" ")
+#     if(i==4):
+#         break
+# for i in range(1,11):
+#     if(i%2==1):
+#         continue
+#     print(i,end=" ")
+
+# inp2=int(input("Enter the number: "))
+
+# for i in range(2,inp2):
+#     if(inp2%i==0):
+#         print("Number is not prime")
+#         break
+i=1
+# while(i<10):
+#     print(i)
+#     i=i+1
+    
+# def greet(str1):
+#     print("Good Morning",str1)
+
+# greet("Shivam")
+
+# def addTwoNum(num1,num2):
+#     print("The sum is: ",(num1+num2))
+
+# addTwoNum(4,8)
+
+def areaOfCircle(radious):
+    area=3.14*radious*radious
+    # print("The area is: ",area)
+    return area
+
+
+# print(areaOfCircle(7))
+
+# my_list = ['apple', 'banana', 'cherry']
+
+# my_list[2]="Rohit"
+# print(my_list[2])
+# my_list.append(78)
+# print(my_list)
+
+# list1=[i for i in range(10)]
+# print(list1)
+
+str1="Adarsh"
+age=23
+
+# print("My name is",str1,"age is: ",age)
+# print("My name is",str1,"age is: ",age)
+print(f"my name {{}} is: {str1} age is: {age}")
+
+# print("My name is {1} anmd age is: {0}".format(age,str1))
+# print("My name is {1} anmd age is: {0}".format(age,str1))
